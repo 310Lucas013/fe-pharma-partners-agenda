9be4bcd38f14f1dca8a9be0b9c97f7e058915c38
Luuk Vermeer <l.vermeer@student.fontys.nl>
2021-02-11 14:55:37 +0100

    pretty much kinda finished the styling of the modal.

--- src/app/agenda/main-calendar/appointment-modal/appointment-modal.component.ts
+++ src/app/agenda/main-calendar/appointment-modal/appointment-modal.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentModalComponent implements OnInit {
 
+  attentionLineText: string;
+
   constructor() { }
 
   ngOnInit(): void {
