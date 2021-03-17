364a691fd4be5eb62b9f1fdfb1a7e4c599bc9c00
Sander Berntsen <380685@student.fontys.nl>
2021-03-17 11:08:16 +0100

    setup login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { Credentials } from 'src/models/Credentials';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,9 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  credentials = {} as Credentials
+
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
@@ -17,4 +21,9 @@ export class LoginComponent implements OnInit {
     this.router.navigate(['/home']);
   }
 
+  login(cre : Credentials) {
+    this.authService.login(cre)
+      .subscribe(data => this.router.navigate(['/home']),
+        error => console.log(error));
+  }
 }
---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import decode from 'jwt-decode';
+import { Credentials } from 'src/models/Credentials';
+
+const AUTH_API = 'http://localhost:4000/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login(credentials: Credentials): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'authenticate',
+      {
+        Username: credentials.username,
+        Password: credentials.password,
+      },
+      httpOptions
+    );
+  }
+}
