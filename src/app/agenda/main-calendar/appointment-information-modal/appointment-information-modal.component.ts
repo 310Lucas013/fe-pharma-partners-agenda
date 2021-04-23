0d3a61d20d07d046eee2124032c108ad5ba8e925
i389746 <389746@stundent.fontys.nl>
2021-04-23 15:25:55 +0200

    frontend design agenda employee

--- src/app/agenda/main-calendar/appointment-information-modal/appointment-information-modal.component.ts
+++ src/app/agenda/main-calendar/appointment-information-modal/appointment-information-modal.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {AppointmentService} from '../../../shared/services/appointment/appointment.service';
+import {Appointment} from '../../../shared/models/appointment';
+import {AppointmentEditModalComponent} from '../appointment-edit-modal/appointment-edit-modal.component';
+import {DateAdapter} from '@angular/material/core';
+import {CustomDateAdapter} from '../../../shared/pipes/custom-date-adapter';
 
 @Component({
   selector: 'app-appointment-information-modal',
   templateUrl: './appointment-information-modal.component.html',
-  styleUrls: ['./appointment-information-modal.component.css']
+  styleUrls: ['./appointment-information-modal.component.css'],
+  providers: [{provide: DateAdapter, useClass: CustomDateAdapter }]
 })
 export class AppointmentInformationModalComponent implements OnInit {
 
-  constructor(private appointmentservice: AppointmentService) { }
+  constructor(appointmentEditModal: AppointmentEditModalComponent) { }
 
-  test: any = 'hello you';
+  @Input() appointment: Appointment;
 
   appointmentTime: string;
 
   ngOnInit(): void {
-
+    console.log(this.appointment);
   }
 
-  appointmentInformation(): void{
+  edit(): void{
+
   }
 
 
