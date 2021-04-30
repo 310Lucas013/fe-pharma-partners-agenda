6f5355e13011732124d48c91171f1ff7cb1ec2ea
i383656 <s.westerburger@student.fontys.nl>
2021-04-30 11:17:53 +0200

    Fixed 2 hour difference because of timezones

--- src/app/agenda/main-calendar/appointment-add-modal/appointment-add-modal.component.ts
+++ src/app/agenda/main-calendar/appointment-add-modal/appointment-add-modal.component.ts
@@ -1,14 +1,12 @@
 import {Component, OnInit, Output, TemplateRef, ViewChild, EventEmitter, Input} from '@angular/core';
-import {CalendarEvent, CalendarEventAction} from 'angular-calendar';
+import {CalendarEvent} from 'angular-calendar';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {Appointment} from '../../../shared/models/appointment';
 import {AppointmentService} from 'src/app/shared/services/appointment/appointment.service';
-import {DatePipe} from '@angular/common';
-import {timeInterval} from 'rxjs/operators';
-import {TimeNumbersPipe} from 'src/app/shared/pipes/time-numbers-pipe';
 import {TokenStorageService} from '../../../shared/services/token-storage/token-storage.service';
 import {AppointmentType} from '../../../shared/models/appointment-type';
 import {ReasonType} from '../../../shared/models/reason-type';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-appointment-add-modal',
@@ -25,20 +23,20 @@ export class AppointmentAddModalComponent implements OnInit {
 
   @Output() addAppointmentEvent = new EventEmitter<Appointment>();
 
-  colors: any = {
-    red: {
-      primary: '#ad2121',
-      secondary: '#FAE3E3',
+  colors = [
+    {
+      name: 'yellow',
+      value: '#ffff00'
     },
-    blue: {
-      primary: '#1e90ff',
-      secondary: '#D1E8FF',
+    {
+      name: 'red',
+      value: '#ff3300'
     },
-    yellow: {
-      primary: '#e3bc08',
-      secondary: '#FDF1BA',
-    },
-  };
+    {
+      name: 'blue',
+      value: '#0000ff'
+    }
+  ];
 
   modalData: {
     action: string;
@@ -48,9 +46,12 @@ export class AppointmentAddModalComponent implements OnInit {
   @ViewChild('modalContent', {static: true}) modalContent: TemplateRef<any>;
 
   // todo change the modal private to the modal of the parent
-  constructor(private modal: NgbModal, private appointmentService: AppointmentService, private tokenService: TokenStorageService) {
+  constructor(private modal: NgbModal, private appointmentService: AppointmentService, private activedRoute: ActivatedRoute) {
     this.appointment.appointmentType = {} as AppointmentType;
     this.appointment.reasonType = {} as ReasonType;
+    activedRoute.params.subscribe(params => {
+      this.employeeId = +params['id'];
+    });
   }
 
   ngOnInit(): void {
@@ -58,6 +59,7 @@ export class AppointmentAddModalComponent implements OnInit {
     this.appointment.startTime = new Date();
     this.appointment.endTime = new Date();
     this.appointment.endTime.setTime(0);
+    this.appointment.color = this.colors[0].value;
   }
 
   handleEvent(action: string, event: CalendarEvent): void {
@@ -70,23 +72,24 @@ export class AppointmentAddModalComponent implements OnInit {
   }
 
   saveAppointment(): void {
-    if(!this.setAppointmentTimes()){
+    if (!this.setAppointmentTimes()) {
       this.error = "Tijden niet goed ingevuld."
       return;
     }
 
+    this.appointment.employeeId = +this.employeeId;
     // TODO set patient and location if from respective services
-    if (this.employeeId === null || this.employeeId === undefined) {
-      this.employeeId = 1;
+    if (this.appointment.employeeId === null || this.appointment.employeeId === undefined) {
+      this.error = "Medewerker onbekend."
+      return;
     }
-    if (this.appointment.patientId === null || this.appointment.patientId  === undefined) {
+    if (this.appointment.patientId === null || this.appointment.patientId === undefined) {
       this.appointment.patientId = 1;
     }
-    if (this.appointment.locationId === null || this.appointment.locationId  === undefined) {
+    if (this.appointment.locationId === null || this.appointment.locationId === undefined) {
       this.appointment.locationId = 1;
     }
 
-    this.appointment.employeeId = this.employeeId;
     this.appointmentService.addAppointment(this.appointment).subscribe(
       data => {
         location.reload();
@@ -100,17 +103,28 @@ export class AppointmentAddModalComponent implements OnInit {
 
   setAppointmentTimes(): boolean {
     if (this.startTime.includes(':') && this.endTime.includes(':')) {
+
       let startHours = Number(this.startTime.split(':')[0]);
       let startMin = Number(this.startTime.split(':')[1]);
       let endHours = Number(this.endTime.split(':')[0]);
       let endMin = Number(this.endTime.split(':')[1]);
+
       this.appointment.startTime = new Date();
       this.appointment.endTime = new Date();
       this.appointment.startTime.setHours(startHours);
       this.appointment.startTime.setMinutes(startMin);
       this.appointment.endTime.setHours(endHours);
       this.appointment.endTime.setMinutes(endMin);
+
+      this.appointment.date = this.convertTZ(this.appointment.date, Intl.DateTimeFormat().resolvedOptions().timeZone)
+      this.appointment.startTime = this.convertTZ(this.appointment.startTime, Intl.DateTimeFormat().resolvedOptions().timeZone)
+      this.appointment.endTime = this.convertTZ(this.appointment.endTime, Intl.DateTimeFormat().resolvedOptions().timeZone)
+
       return this.appointment.endTime > this.appointment.startTime;
     }
+  }
+
+  convertTZ(date: any, tzString: string) {
+    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));
   }
 }
