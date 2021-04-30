fa8f459cd42f6b7e4458ad86b7ef5265a12ffc9c
Kellyvdhoek <k.vanderhoek@student.fontys.nl>
2021-04-30 14:19:25 +0200

    colors fixxxx
    fixed the colors from the merge

--- src/app/agenda/main-calendar/appointment-add-modal/appointment-add-modal.component.ts
+++ src/app/agenda/main-calendar/appointment-add-modal/appointment-add-modal.component.ts
@@ -26,6 +26,21 @@ export class AppointmentAddModalComponent implements OnInit {
 
   @Output() addAppointmentEvent = new EventEmitter<Appointment>();
 
+  colors: any = {
+    red: {
+      primary: '#ad2121',
+      secondary: '#FAE3E3',
+    },
+    blue: {
+      primary: '#1e90ff',
+      secondary: '#D1E8FF',
+    },
+    yellow: {
+      primary: '#e3bc08',
+      secondary: '#FDF1BA',
+    },
+  };
+
   modalData: {
     action: string;
     event: CalendarEvent;
