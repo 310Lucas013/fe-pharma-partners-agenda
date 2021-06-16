92f4c45894cada695e19a4ff6edce0cad1c71ca7
i383656 <s.westerburger@student.fontys.nl>
2021-06-16 10:48:05 +0200

    Authorization

--- src/app/shared/services/appointment/appointment.service.ts
+++ src/app/shared/services/appointment/appointment.service.ts
@@ -3,51 +3,50 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Appointment} from '../../models/appointment';
 import {Observable} from 'rxjs';
 import {environment} from '../../../../environments/environment';
-import {AppointmentDto} from '../../dto/appointment-dto';
+import {TokenStorageService} from '../token-storage/token-storage.service';
 
 const API_KEY = environment.gatewayApi + 'appointments';
 
-const httpOptions = {
-  headers: new HttpHeaders({'Content-Type': 'application/json'}),
-};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
 
-  constructor(
-    private http: HttpClient
-  ) {
-  }
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json',
+      Authorization: this.tokenStorage.getToken()}),
+  };
+
+  constructor(private http: HttpClient, private tokenStorage: TokenStorageService) { }
+
 
   addAppointment(appointment: Appointment): Observable<any> {
-    return this.http.post<Appointment>(API_KEY + '/create', appointment, httpOptions);
+    return this.http.post<Appointment>(API_KEY + '/create', appointment, this.httpOptions);
   }
 
   getAppointmentsByEmployeeId(employeeId: number): Observable<Appointment[]> {
-    return this.http.get<Appointment[]>(API_KEY + '/employee-id/' + employeeId);
+    return this.http.get<Appointment[]>(API_KEY + '/employee-id/' + employeeId, this.httpOptions);
   }
 
   deleteAppointment(appointmentId: number): Observable<any> {
-    const aaa = API_KEY + '/' + appointmentId;
-    return this.http.delete(aaa);
+    return this.http.delete(API_KEY + '/' + appointmentId, this.httpOptions);
   }
 
   updateAppointment(appointment: Appointment): Observable<any> {
     console.log(appointment);
-    return this.http.put<Appointment>(API_KEY + '/update/', JSON.stringify(appointment), httpOptions);
+    return this.http.put<Appointment>(API_KEY + '/update/', JSON.stringify(appointment), this.httpOptions);
   }
 
   setAppointmentStatusAbsent(appointmentId: number): Observable<any> {
-    return this.http.get<any>(API_KEY + '/absent/' + appointmentId);
+    return this.http.get<any>(API_KEY + '/absent/' + appointmentId, this.httpOptions);
   }
 
   setAppointmentStatusRegistered(appointmentId: number): Observable<any> {
-    return this.http.get<any>(API_KEY + '/registered/' + appointmentId);
+    return this.http.get<any>(API_KEY + '/registered/' + appointmentId, this.httpOptions);
   }
 
   setAppointmentStatusDone(appointmentId: number): Observable<any> {
-    return this.http.get<any>(API_KEY + '/done/' + appointmentId);
+    return this.http.get<any>(API_KEY + '/done/' + appointmentId, this.httpOptions);
   }
 }
--- src/app/shared/services/patient/patient.service.ts
+++ src/app/shared/services/patient/patient.service.ts
@@ -13,7 +13,7 @@ export class PatientService {
 
   public API_KEY = environment.gatewayApi + 'patients';
 
-  public httpOptions = {
+  httpOptions = {
     headers: new HttpHeaders({'Content-Type': 'application/json',
       Authorization: this.tokenStorage.getToken()}),
   };
